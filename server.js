5ded3f4c4edeacbb98d4507f35481c14d9e8cd70
Glitch (bristle-feast) <none>
2018-03-24 10:31:57 +0000

    😾🎰 Updated with Glitch

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function handlePar(param) {
     return printResult(unix, nat);
 }
 
-// listen for requests :)
+// listen for requests
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
